9d45053d0824ff53117c3d43dcf441f77e9c7a03
Asad Ali Sidow <sidow.code@gmail.com>
2022-07-16 12:56:47 +0300

    react hook form added in authentication pages

--- authentication_pages/src/pages/SignIn.tsx
+++ authentication_pages/src/pages/SignIn.tsx
@@ -1,12 +1,34 @@
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { object, string, Asserts } from "yup";
+
+const schema = object({
+    email: string().email().required(),
+    password: string().required(),
+});
+
 export default function SignIn() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Asserts<typeof schema>>({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
     return (
         <div className="flex flex-col justify-center items-center h-full">
             <div className="bg-white w-2/6 rounded-xl p-3 drop-shadow-md">
                 <h1 className="text-center text-2xl font-semibold mt-5"> Welcome Back </h1>
                 <p className="text-center text-sm font-light mt-3"> Enter your credential to access your account. </p>
 
-                <form action="" className="px-3 mt-10 mb-5">
-                    <div className="form-group mb-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="px-3 mt-10 mb-5">
+                    <div className="form-group mb-3">
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             Email
                         </label>
@@ -18,15 +40,17 @@ export default function SignIn() {
                                 </svg>
                             </div>
                             <input
-                                type="text"
+                                type="email"
                                 id="email"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="name@flowbite.com"
+                                {...register("email")}
                             />
                         </div>
+                        {errors.email && <div className="text-red-400 ml-1"> {errors.email?.message} </div>}
                     </div>
 
-                    <div className="form-group mb-5">
+                    <div className="form-group mb-3">
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             Password
                         </label>
@@ -44,17 +68,24 @@ export default function SignIn() {
                                 id="password"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="password"
+                                {...register("password")}
                             />
                         </div>
+                        {errors.password && <div className="text-red-400 ml-1"> {errors.password?.message} </div>}
                     </div>
 
-                    <button type="button" className="text-white w-full mt-5 bg-[#2680FE] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-[#2680FE] focus:outline-none dark:focus:ring-blue-800">
+                    <button type="submit" className="text-white w-full mt-5 bg-[#2680FE] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-[#2680FE] focus:outline-none dark:focus:ring-blue-800">
                         Sign In
                     </button>
                 </form>
             </div>
 
-            <p className="mt-10 text-sm font-light"> I don't have an account <a href="#" className="text-[#2680FE] font-semibold">Register Now</a></p>
+            <p className="mt-10 text-sm font-light">
+                I don't have an account?
+                <Link to="/sign-up" className="text-[#2680FE] font-semibold ml-1">
+                    Register Now
+                </Link>
+            </p>
         </div>
     );
 }
